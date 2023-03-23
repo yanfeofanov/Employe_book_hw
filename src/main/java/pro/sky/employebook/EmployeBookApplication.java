01ea96cd94546a7d68089805f261295a9f481843
kaVII-x <ka52jiguli@yandex.ru>
2023-03-23 22:09:29 +0200

    Первая сборка проекта

---
+++ src/main/java/pro/sky/employebook/EmployeBookApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.employebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeBookApplication.class, args);
+	}
+
+}
