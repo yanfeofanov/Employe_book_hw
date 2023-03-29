40c88fd5b9e4fcb1d877492d3b7aa67626c66579
kaVII-x <ka52jiguli@yandex.ru>
2023-03-29 04:22:36 +0200

    Написание методов добавление, удаление, поиск и реализация Exception для нет таких сотрудников

--- src/main/java/pro/sky/employebook/Employee.java
+++ src/main/java/pro/sky/employebook/Employee.java
@@ -39,9 +39,8 @@ public class Employee {
 
     @Override
     public String toString() {
-        return "Employee{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                '}';
+        return
+                getFirstName() +
+                        " " + getLastName() + " ";
     }
 }
--- src/main/java/pro/sky/employebook/EmployeeController.java
+++ src/main/java/pro/sky/employebook/EmployeeController.java
@@ -1,12 +1,43 @@
 package pro.sky.employebook;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class EmployeeController {
-    @GetMapping(path = "/hello")
-    public String hello(){
-        return "hello";
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
+
+    @GetMapping(path = "/info")
+    public String getEmployeeInfo() {
+        return employeeService.getInfo().toString();
+    }
+
+    @GetMapping(path = "/info/add")
+    public Employee addEmployee(@RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName) {
+        return employeeService.addEmployee(firstName, lastName);
+    }
+
+    @GetMapping(path = "/info/remove")
+    public Employee removeEmployee(@RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName) {
+
+        return employeeService.removeEmployee(firstName, lastName);
+    }
+
+    @GetMapping(path = "/info/find")
+    public Employee findEmployee(@RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName) {
+        return employeeService.findEmployee(firstName, lastName);
+    }
+
+
+
+
+
 }
